b5b20ec75cb0a37e677220ab8567328db8342d50
kimsungkyum <sungkyum1@naver.com>
2023-02-03 01:02:35 +0900

    HOTFIX!:API KEY 숨기기

--- lib/data.dart
+++ lib/data.dart
@@ -1,5 +1,3 @@
 import 'package:flutter/material.dart';
 
-const String android_google_api_key = "AIzaSyCU9jBlUPlU6Qf93YYUImNqelaZqvsmEec";
-const String ios_google_api_key = "AIzaSyBRb1yW11eFkGr5BYYqLwvCEn1U3xIOAlw";
 const Color primaryColor = Color(0X5CDEB7);
